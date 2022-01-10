cb8f760e93ef89bd91904269a3548c2d42180a3a
Bruno Luvizotto Carli <bruno.carli@smartgreen.net>
2022-01-10 19:56:18 -0300

    CHANGE backend url and secret key

--- hippocrates/settings.py
+++ hippocrates/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'emb^y7or*%3$&ltgi$(o^uqecabmb&f&hv$xlccp*ch=pzke#t'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
