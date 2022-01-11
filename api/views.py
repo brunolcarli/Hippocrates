e498cfb0b8527e51eeae7def9f81b8f7e442b522
Bruno L. Carli <brunolcarli@gmail.com>
2022-01-11 00:23:07 -0300

    CHANGE get backend host from env

--- api/views.py
+++ api/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.conf import settings
 
 def index(request):
 	"""Homepage"""
+	host = settings.BACKEND['host']
+	port = settings.BACKEND['port']
+	env_ref = settings.ENV_REF
 	return render(request, 'pages/index.html')
